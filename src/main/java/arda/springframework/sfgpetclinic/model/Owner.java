86c975a7a88d9ae3a8d7b883786ab79ed3e6530a
ardatosun <t.arda@outlook.com>
2019-01-23 22:52:01 +0300

    Added POJOs. Closes Issue #2.

---
+++ src/main/java/arda/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package arda.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/arda/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package arda.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
