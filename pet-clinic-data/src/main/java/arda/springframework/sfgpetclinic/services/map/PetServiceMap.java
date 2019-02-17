357ec475e893c224ab6504bf837915d411905764
ardatosun <t.arda@outlook.com>
2019-02-17 21:57:42 +0300

    Implement Map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package arda.springframework.sfgpetclinic.services.map;
+
+import arda.springframework.sfgpetclinic.model.Pet;
+import arda.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
