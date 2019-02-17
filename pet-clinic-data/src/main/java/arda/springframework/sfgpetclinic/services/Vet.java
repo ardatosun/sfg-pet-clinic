7bd820641a4019d98877ac267cf67406c10fafd1
ardatosun <t.arda@outlook.com>
2019-02-17 19:25:45 +0300

    Refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/Vet.java
+++ pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/Vet.java
@@ -1,12 +1,5 @@
 package arda.springframework.sfgpetclinic.services;
 
-import java.util.Set;
+public interface Vet extends CrudService<Vet, Long>{
 
-public interface Vet {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
