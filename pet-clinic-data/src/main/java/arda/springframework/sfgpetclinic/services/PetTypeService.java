f0346b21f4fc150de8a71d083d77058faeff36f7
ardatosun <t.arda@outlook.com>
2019-03-11 21:50:49 +0300

    Added PetType service. Closes #29

---
+++ pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package arda.springframework.sfgpetclinic.services;
+
+import arda.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
