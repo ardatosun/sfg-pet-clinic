6a5b24d256a2991a0dff92534d7047b915f863aa
ardatosun <t.arda@outlook.com>
2019-03-11 22:16:10 +0300

    Added Specialty Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,36 @@
+package arda.springframework.sfgpetclinic.services.map;
+
+import arda.springframework.sfgpetclinic.model.Speciality;
+import arda.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
