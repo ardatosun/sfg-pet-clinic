8a87a9a9d6ef3113b3bae29e878ffaed16cc16e2
ardatosun <t.arda@outlook.com>
2019-03-11 22:32:34 +0300

    Added Specialities to Vets on startup. Closes #34

--- pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/arda/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package arda.springframework.sfgpetclinic.services.map;
 
+import arda.springframework.sfgpetclinic.model.Speciality;
 import arda.springframework.sfgpetclinic.model.Vet;
+import arda.springframework.sfgpetclinic.services.SpecialtyService;
 import arda.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialtyService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
